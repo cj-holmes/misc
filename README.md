cdb761d2ba4008b8afc25fc9f32f66482c0b07c1
Dickie <c.holmes4@gmail.com>
2018-09-14 22:12:18 +0100

    Add R colours code and comment

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 Some odd bits and bobs that I wanted to get version controlled and accessible from anywhere. Nothing good, but it will also help me use Git a little more.
 
 ### PCH map
-A helpful chart for referencing pch values and shapes ![reference plot](pch_ref_plot/pch_reference_plot.png)
+A helpful chart for referencing pch values and shapes ![PCH reference plot](pch_ref_plot/pch_reference_plot.png)
+
+### R colours
+A reference image of the base R colours ![R base colours](r_colours/R_colours.png)
--- r_colours/r_colours.R
+++ r_colours/r_colours.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 
+# Construct tibble containing base colours and their RGB/HSV values
+# Order by HSV so its a bit more useful...
 ordered <-
   tibble(colour_names = colours()) %>%
   mutate(rgb = map(colour_names, col2rgb),
@@ -9,13 +11,16 @@ ordered <-
          val = map_dbl(hsv, ~.x[3])) %>%
   arrange(hue, sat, val)
 
+# Assign variables for plot
 l <- length(colours())
 w <- 10 # number of columns in plot
 h <- ceiling(l/w)
 
+# Create plot of colours
 as.tibble(expand.grid(y = 1:h, x = 1:w)) %>%
   slice(1:l) %>% 
   mutate(fill_colour = ordered$colour_names,
+         # assign a text colour that will be readable on the background colour
          text_colour = map_chr(fill_colour, ~ifelse(mean(col2rgb(.x)) > 127, 
                                                     "black",
                                                     "white"))) %>%
@@ -26,4 +31,5 @@ as.tibble(expand.grid(y = 1:h, x = 1:w)) %>%
   scale_colour_identity()+
   theme_void()
 
+# Produce a4 visualisation of colours
 ggsave("r_colours/R_colours.png", height = 297, width = 210, units = "mm")
